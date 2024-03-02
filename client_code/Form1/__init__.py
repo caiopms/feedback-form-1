a1dab301171bf73346180a3bbda55edc1e219116
linknos@gmail.com <linknos@gmail.com>
2024-03-02 18:33:39 +0000

    Created Form1, Form1.ItemTemplate1, Form1.RowTemplate1, Form1.RowTemplate2 and 1 more; Edited settings; Deleted HomeForm

---
+++ client_code/Form1/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import Form1Template
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def date_picker_1_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    pass
